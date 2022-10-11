71e084a44fcf423f2bd852ca76e8aed2ec03827e
ajankowi <252919@student.pwr.edu.pl>
2022-10-11 19:05:11 +0200

    Lab2

--- Lab1/Lab1.m
+++ Lab1/Lab1.m
@@ -7,3 +7,28 @@ th3 = 0.24; %kg*m^2
 th4 = 0.77; %kg*m
 
 g = 9.81;   %m/s^2
+
+N = 9;      %Ostatni numer indeksu
+
+tol = 1e-10; %tolerancja
+time = 30.0;  %time
+
+Kp11 = -1000;
+Kp22 = Kp11;
+
+Kd11 = -100;
+Kd22 = Kd11;
+
+
+
+
+
+
+
+Kp11_2 = -10;
+Kp22_2 = Kp11;
+
+Kd11_2 = -1;
+Kd22_2 = Kd11;
+
+
