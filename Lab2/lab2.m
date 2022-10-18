90abb2cf4f1d03e7f78112a20504f637bb7d4adb
ajankowi <252919@student.pwr.edu.pl>
2022-10-18 20:43:20 +0200

    Lab2

---
+++ Lab2/lab2.m
@@ -0,0 +1,46 @@
+clear all;
+close all;
+
+th1 = 3.1;  %kg*m^2
+th2 = 9.5;  %kg*m
+th3 = 0.24; %kg*m^2
+th4 = 0.77; %kg*m
+
+g = 9.81;   %m/s^2
+
+N = 9;      %Ostatni numer indeksu
+
+tol = 1e-10; %tolerancja
+time = 1000.0;  %time
+
+
+
+Kd = 100;
+Kp = 10;
+
+
+
+
+
+
+
+
+Kp11 = -1000;
+Kp22 = Kp11;
+
+Kd11 = -100;
+Kd22 = Kd11;
+
+
+
+
+
+
+
+Kp11_2 = -10;
+Kp22_2 = Kp11;
+
+Kd11_2 = -1;
+Kd22_2 = Kd11;
+
+
