19ea8fd796e0111075a13c1358f058faae2e8ca3
ajankowi <252919@student.pwr.edu.pl>
2022-11-25 16:18:18 +0100

    Lab4 - monocykl

--- Lab3/Lab3.m
+++ Lab3/Lab3.m
@@ -9,3 +9,35 @@ vd = R*wd;
 
 k1 = 10;
 k2 = 100;
+
+kd = 100;
+
+Mp = 97; %Kg
+Mk = 5;  %Kg
+
+Ip = 6.609; %kg*m^2
+
+Rk = 0.075; %m
+L = 0.3;    %m
+
+Mc = Mp + 2 * Mk; %kg
+
+Izz = (1/4) * Mk * L^2;             %kg*m^2
+Ic = Ip + 2 * Izz + 2 * Mk * L^2;   %kg*m^2
+Ixx = (1/2) * Mk * Rk^2;            %kg*m^2
+
+
+M = [Mc+(2*Ixx/Rk^2),0;0,Ic+(2*Ixx*L^2/Rk^2)];
+
+M_1 = M^(-1);
+
+inv(M);
+
+B = [1/Rk,1/Rk;L/Rk,-L/Rk];
+
+B_1 = B^(-1);
+
+
+
+
+
